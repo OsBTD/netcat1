2a1d9dd27b3f002dc6cf7cb0643b1093dd37f444
Oussama Atmani <borntolearn314@gmail.com>
2024-12-02 06:40:53 +0100

    dd

--- main.go
+++ main.go
@@ -34,7 +34,15 @@ func main() {
 }
 
 func Handle(conn net.Conn) {
+	conn.Write([]byte("Welcome to TCP-Chat!\n         _nnnn_\n        dGGGGMMb\n       @p~qp~~qMb\n       M|@||@) M|\n       @,----.JM|\n      JS^\\__/  qKL\n     dZP        qKRb\n    dZP          qKKb\n   fZP            SMMb\n   HZM            MMMM\n   FqM            MMMM\n __| \".        |\\dS\"qML\n |    `.       | `' \\Zq\n_)      \\.___.,|     .'\n\\____   )MMMMMP|   .'\n     `-'       `--'\n[ENTER YOUR NAME]: "))
+
 	clientgone := "client has disconnected"
+	// bufferName := make([]byte, 1024)
+	// var users map[string]net.Conn
+	// m, _ := conn.Read(bufferName)
+
+	// username := bufferName[:m]
+	// users = append(users, username)
 	for {
 		buffer := make([]byte, 1024)
 		n, err := conn.Read(buffer)
